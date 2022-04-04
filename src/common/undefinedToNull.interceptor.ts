ab3112bd5e9a820990adeafbac4aa3843a84465e
chanminjo <happyjarban@gmail.com>
2022-04-05 00:45:01 +0900

    style: linting

--- src/common/undefinedToNull.interceptor.ts
+++ src/common/undefinedToNull.interceptor.ts
@@ -12,6 +12,8 @@ export class UndefinedToNullInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(map(data => (data === undefined ? null : data)));
+    return next
+      .handle()
+      .pipe(map((data) => (data === undefined ? null : data)));
   }
 }
