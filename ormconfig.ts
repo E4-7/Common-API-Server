9e51dc4a4dfa4300f8cdeba09217854097059f55
chanminjo <happyjarban@gmail.com>
2022-04-04 20:23:05 +0900

    feat: ormconfig 추가

---
+++ ormconfig.ts
@@ -0,0 +1,28 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+declare type TypeOrmSeedingOptions = {
+  seeds: Array<string>;
+  factories: Array<string>;
+};
+const config: TypeOrmModuleOptions & TypeOrmSeedingOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/src/common/database/migrations/*.ts'],
+  cli: {migrationsDir: 'src/common/database/migrations'},
+  seeds: ['src/common/database/seeds/**/*.seed.ts'], // seed파일들을 생성할 경로
+  factories: ['src/common/database/factories/**/*.factory.ts'],
+  autoLoadEntities: true,
+  charset: 'utf8mb4',
+  synchronize: false,
+  logging: process.env.NODE_ENV !== 'production',
+  keepConnectionAlive: true,
+};
+
+export = config;
