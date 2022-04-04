827d88d1a19e57a75ba76e06e92727f99285c72f
chanminjo <happyjarban@gmail.com>
2022-04-05 00:45:26 +0900

    docs: common.entity.ts swagger 적용

--- src/common/abstract/common.entity.ts
+++ src/common/abstract/common.entity.ts
@@ -1,12 +1,36 @@
-import {CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn,} from 'typeorm';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export abstract class CommonEntity {
-  @PrimaryGeneratedColumn({type: 'int', name: 'id'})
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '1',
+    description: '고유 id',
+  })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '2022-04-04 20:17:54.032942',
+    description: '생성일시',
+  })
   @CreateDateColumn()
   createdAt: Date;
 
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '2022-04-04 20:17:54.032942',
+    description: '수정일시',
+  })
   @UpdateDateColumn()
   updatedAt: Date;
 }
