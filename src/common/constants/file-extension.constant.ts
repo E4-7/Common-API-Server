0d76c0f4582c350135ec05586ef9cff84c123bf2
chanminjo <happyjarban@gmail.com>
2022-04-13 01:30:41 +0900

    feat: 파일 extension,size enum 추가 및 파일이름변경

---
+++ src/common/constants/file-extension.constant.ts
@@ -0,0 +1,13 @@
+export enum Mimetype {
+  JPEG = 'image/jpeg',
+  JPG = 'image/jpeg',
+  PNG = 'image/png',
+  WEBP = 'image/webp',
+  GIF = 'image/gif',
+  PDF = 'application/pdf',
+  ZIP = 'application/zip',
+}
+
+export enum FileSize {
+  _10MB = 10485760,
+}
