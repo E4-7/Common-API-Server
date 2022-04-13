9ef7e4c80c469cf49f5696ce93ab316c3a588788
chanminjo <happyjarban@gmail.com>
2022-04-14 03:05:09 +0900

    fix: s3-mock promise then떄문에 dependency 안되는 오류 해결 HIGHLIGHT: 기본에 충실하자 promise에서 의미없이 then,catch를 인자로 넣어서 timeout이 자꾸 발생함.

--- src/common/constants/s3-mock.constant.ts
+++ src/common/constants/s3-mock.constant.ts
@@ -2,8 +2,6 @@ const mockS3 = () => ({
   upload: jest.fn().mockReturnThis(),
   deleteObject: jest.fn().mockReturnThis(),
   promise: jest.fn().mockReturnThis(),
-  then: jest.fn(),
-  error: jest.fn(),
 });
 
 export { mockS3 };
