cdf4285504716ef4910233a70ff61ff3e1b71269
chanminjo <happyjarban@gmail.com>
2022-04-14 03:06:31 +0900

    test: file upload, delete service 테스트 코드 추가

--- src/modules/files/files.service.spec.ts
+++ src/modules/files/files.service.spec.ts
@@ -10,6 +10,15 @@ import { mockS3 } from '../../common/constants/s3-mock.constant';
 import { ConfigService } from '@nestjs/config';
 import { S3 } from 'aws-sdk';
 import { mockFileUpload, oneFile } from './files.mock';
+import {
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  NOT_FOUND_FILE_KEY,
+  UNKNOWN_ERR,
+} from '../../common/constants/error.constant';
 
 describe('FilesService', () => {
   let fileService: FilesService;
@@ -21,6 +30,7 @@ describe('FilesService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         FilesService,
+        Logger,
         {
           provide: getRepositoryToken(Files),
           useValue: mockRepository(),
@@ -46,6 +56,14 @@ describe('FilesService', () => {
   });
 
   describe('uploadFile', () => {
+    beforeAll(() => {
+      jest.useFakeTimers('modern');
+      jest.setSystemTime(new Date(2020, 3, 1));
+    });
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
     it('파일 업로드(정상 작동)', async () => {
       fileRepository.save.mockResolvedValue({
         mockFileUpload,
@@ -57,20 +75,56 @@ describe('FilesService', () => {
 
       const response = await fileService.uploadFile(oneFile);
 
-      expect(s3Service.upload).toHaveProperty(
-        'Bucket',
-        configService.get('file.aws_s3_bucket_name'),
-      );
-      expect(s3Service.upload).toHaveProperty('Key');
-      expect(s3Service.upload).toHaveProperty('Body', oneFile.buffer);
-      expect(s3Service.upload).toHaveProperty('ContentType', oneFile.mimetype);
       expect(s3Service.promise).toHaveBeenCalledTimes(1);
+      expect(s3Service.upload).toHaveBeenCalledWith({
+        Bucket: configService.get('file.aws_s3_bucket_name'),
+        Key: `files/${new Date().valueOf()}/${oneFile.originalname}`,
+        Body: oneFile.buffer,
+        ContentType: oneFile.mimetype,
+      });
+      expect(fileRepository.save).toHaveBeenCalledWith({
+        key: mockFileUpload.key,
+        size: oneFile.size,
+        mimetype: oneFile.mimetype,
+        original_name: oneFile.originalname,
+        url: mockFileUpload.url,
+      });
       expect(fileRepository.save).toHaveBeenCalledTimes(1);
-      expect(response).toEqual(oneFile);
+      expect(response).toStrictEqual({ mockFileUpload });
+    });
+
+    it('파일 업로드(s3 업로드, 저장소 저장 실패)', () => {
+      fileRepository.save.mockResolvedValue({
+        mockFileUpload,
+      });
+      s3Service.promise = jest.fn().mockImplementation(() => {
+        throw new InternalServerErrorException();
+      });
+      expect(fileService.uploadFile(oneFile)).rejects.toThrowError(
+        new InternalServerErrorException(UNKNOWN_ERR),
+      );
     });
   });
 
   describe('deleteFile', () => {
+    it('파일 삭제(키가 없을 경우)', () => {
+      fileRepository.findOne.mockResolvedValue(null);
+
+      expect(fileService.deleteFile(mockFileUpload.key)).rejects.toThrowError(
+        new NotFoundException(NOT_FOUND_FILE_KEY),
+      );
+    });
+    it('파일 삭제(키가 없거나, fileRepository delete 안될 경우)', () => {
+      fileRepository.findOne.mockResolvedValue({
+        mockFileUpload,
+      });
+      s3Service.promise = jest.fn().mockImplementation(() => {
+        throw new InternalServerErrorException();
+      });
+      expect(fileService.deleteFile(mockFileUpload.key)).rejects.toThrowError(
+        new InternalServerErrorException(UNKNOWN_ERR),
+      );
+    });
     it('파일 삭제(정상 작동)', async () => {
       fileRepository.findOne.mockResolvedValue({
         mockFileUpload,
